35f2dc4df1bc731c379ba652ff165cd7ffb590d5
Gianissssss <gianicpos@gmail.com>
2022-04-04 11:05:55 -0300

    inicio

--- mi_primer_proyecto/src/app/app-routing.module.ts
+++ mi_primer_proyecto/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasosDeExitoComponent } from './pages/casos-de-exito/casos-de-exito.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio',component:InicioComponent},
+  {path:'',component:InicioComponent},
+  
+  {path:'equipo',component:EquipoComponent},
+  {path:'contacto',component:ContactoComponent},
+  {path:'casos_de_exito',component:CasosDeExitoComponent},
+  {path:'servicios',component:ServiciosComponent},
+  {path:'**',component:InicioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
