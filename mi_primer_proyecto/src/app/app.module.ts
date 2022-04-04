41abea20a5d67553b6e39577733f0d3b779ec7c3
Gianissssss <gianicpos@gmail.com>
2022-04-03 23:52:36 -0300

    proyecto

---
+++ mi_primer_proyecto/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { CasosDeExitoComponent } from './pages/casos-de-exito/casos-de-exito.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    InicioComponent,
+    EquipoComponent,
+    ContactoComponent,
+    CasosDeExitoComponent,
+    ServiciosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ mi_primer_proyecto/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',//etiqueta
+  templateUrl: './footer.component.html',//vista(interfaz)
+  styleUrls: ['./footer.component.css']//estilo
+})
+export class FooterComponent  {
+
+
+}
